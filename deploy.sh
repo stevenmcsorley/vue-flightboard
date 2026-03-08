7d31a4e498bd7f80009a1765b9642de24707eb42
steven <smcsorl@gmail.com>
2026-03-08 20:28:15 +0000

    Clean up dist repo after deploy

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,22 @@
 set -eu
 
 REPO_URL="${REPO_URL:-https://github.com/stevenmcsorley/vue-flightboard}"
+DIST_DIR="$(pwd)/dist"
 
 npm run build
 
-cd dist
+cleanup() {
+  rm -rf "$DIST_DIR/.git"
+}
+
+trap cleanup EXIT
+
+cd "$DIST_DIR"
 
 touch .nojekyll
 cp index.html 404.html
 
+rm -rf .git
 git init
 git add -A
 git commit -m 'deploy'
